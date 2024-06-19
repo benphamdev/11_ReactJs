4a22134c5b5dbf416a4704f22651c0e7646eecbc
benphamdev <duychien152003@gmail.com>
2024-06-19 12:20:29 +0700

    video 20 setState fix error using arrow func

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -10,11 +10,39 @@ class MyComponent extends React.Component {
         age: 21,
     };
 
+    handleClick() {
+        console.log("hello world");
+    }
+
+    handleMouseOver(event) {
+        // console.log(event.pageX);
+        //https:bobbyhadz.com/blog/react-typeerror-cannot-read-property-setstate-of-undefined
+        console.log("My name is", this.state.name, " and i from ", this.state.age);
+
+        this.setState({
+            name: "pham duy chien",
+            age: Math.floor(Math.random() * (100 - 10 + 1) + 10)
+        });
+
+
+        // react class => merge state 
+        this.setState({
+            address: Math.floor(Math.random() * (100 - 10 + 1) + 10),
+        });
+    }
     // jsx
     render() {
         return (
             <div>
                 My name is {this.state.name} and i from {this.state.address}
+                <button onClick={this.handleClick}>On click</button>
+                <button
+                    onMouseOver={(event) => {
+                        this.handleMouseOver(event);
+                    }}
+                >
+                    Mouse over
+                </button>
             </div>
         );
     }
