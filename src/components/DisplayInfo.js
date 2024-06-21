206aa1ce31473d6c0cc70b285e852744814f3bd5
benphamdev <duychien152003@gmail.com>
2024-06-21 08:46:13 +0700

    stateful && stateless component

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,94 +1,136 @@
-import logo from "./../logo.svg";
 import "./DisplayInfo.scss";
-import {Component} from "react";
+import logo from "./../logo.svg";
 
-export default class DisplayInfo extends Component {
-    state = {
-        flag: true,
-    };
+// stateless vs stateful
+// class component
+// stateful
+// export default class DisplayInfo extends Component {
+//     state = {
+//         flag: true,
+//     };
+//
+//     // babel compiler
+//     constructor(props) {
+//         super(props);
+//         console.log("constructor")
+//         this.state = {
+//             flag: true
+//         }
+//     }
+//
+//     componentDidMount() {
+//         console.log("componentDidMount")
+//         setTimeout(() => {
+//             document.title = "Display Info";
+//         }, 3000);
+//     }
+//
+//     componentDidUpdate(prevProps, prevState, snapshot) {
+//         console.log("componentDidUpdate", prevProps, prevState, snapshot);
+//
+//         if (this.props.listUsers !== prevProps.listUsers) {
+//             if (this.props.listUsers.length === 5)
+//                 alert("You got 5 users");
+//         }
+//     }
+//
+//     handleToggle = () => {
+//         this.setState({
+//             flag: !this.state.flag,
+//         });
+//     };
+//
+//     render() {
+//         // props => properties
+//         // console.log(this.props);
+//         // destructuring array
+//
+//         // let { name, age } = this.props;
+//         // return (
+//         //     <div>
+//         //         My name is : {name}
+//         //         <br />
+//         //         My age : {age}
+//         //     </div>
+//         // );
+//
+//         console.log("render")
+//         // destructuring object
+//         const {listUsers} = this.props;
+//         return (
+//             <div className="display-info-container">
+//                 <img src={logo} alt=""/>
+//                 <div>
+// 					<span onClick={() => this.handleToggle()}>
+// 						{this.state.flag ? "Hide" : "Show"} list user :
+// 					</span>
+//                 </div>
+//                 {/* // conditional rendering */}
+//                 {/* note : boolean not render  */}
+//                 {this.state.flag && (
+//                     <>
+//                         {listUsers.map((user, index) => {
+//                             return (
+//                                 <div className={user.age < 30 ? "red" : "green"} key={user.id}>
+//                                     <div style={{color: "purple", paddingTop: "5px"}}>
+//                                         ID : {user.id}
+//                                     </div>
+//                                     <p>Name : {user.name}</p>
+//                                     <p>Address : {user.address}</p>
+//                                     <p>Age : {user.age}</p>
+//                                     <br/>
+//                                     <div>
+//                                         <button onClick={() => this.props.handleDeleteUser(user.id)}>
+//                                             Delete user
+//                                         </button>
+//                                     </div>
+//                                     <hr/>
+//                                 </div>
+//                             );
+//                         })}
+//                     </>
+//                 )}
+//             </div>
+//         );
+//     }
+// }
 
-    // babel compiler
-    constructor(props) {
-        super(props);
-        console.log("constructor")
-        this.state = {
-            flag: true
-        }
-    }
+// stateless
+// function component => don't has state
+const DisplayInfo = (props) => {
+    // destructuring object
+    const {listUsers} = props;
+    return (
+        <div className="display-info-container">
+            <img src={logo} alt=""/>
 
-    componentDidMount() {
-        console.log("componentDidMount")
-        setTimeout(() => {
-            document.title = "Display Info";
-        }, 3000);
-    }
-
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        console.log("componentDidUpdate", prevProps, prevState, snapshot);
-
-        if (this.props.listUsers !== prevProps.listUsers) {
-            if (this.props.listUsers.length === 5)
-                alert("You got 5 users");
-        }
-    }
-
-    handleToggle = () => {
-        this.setState({
-            flag: !this.state.flag,
-        });
-    };
-
-    render() {
-        // props => properties
-        // console.log(this.props);
-        // destructuring array
-
-        // let { name, age } = this.props;
-        // return (
-        //     <div>
-        //         My name is : {name}
-        //         <br />
-        //         My age : {age}
-        //     </div>
-        // );
-
-        console.log("render")
-        // destructuring object
-        const {listUsers} = this.props;
-        return (
-            <div className="display-info-container">
-                <img src={logo} alt=""/>
-                <div>
-					<span onClick={() => this.handleToggle()}>
-						{this.state.flag ? "Hide" : "Show"} list user :
-					</span>
-                </div>
-                {/* // conditional rendering */}
-                {/* note : boolean not render  */}
-                {this.state.flag && (
-                    <>
-                        {listUsers.map((user, index) => {
-                            return (
-                                <div className={user.age < 30 ? "red" : "green"} key={user.id}>
-                                    <div style={{color: "purple", paddingTop: "5px"}}>
-                                        ID : {user.id}
-                                    </div>
-                                    <p>Name : {user.name}</p>
-                                    <p>Address : {user.address}</p>
-                                    <p>Age : {user.age}</p>
-                                    <br/>
-                                    <div>
-                                        <button onClick={() => this.props.handleDeleteUser(user.id)}>
-                                            Delete user
-                                        </button>
-                                    </div>
-                                    <hr/>
+            {/* // conditional rendering */}
+            {/* note : boolean not render  */}
+            {true && (
+                <>
+                    {listUsers.map((user, index) => {
+                        return (
+                            <div className={user.age < 30 ? "red" : "green"} key={user.id}>
+                                <div style={{color: "purple", paddingTop: "5px"}}>
+                                    ID : {user.id}
                                 </div>
-                            );
-                        })}
-                    </>
-                )}
-            </div>
-        );
-    }
+                                <p>Name : {user.name}</p>
+                                <p>Address : {user.address}</p>
+                                <p>Age : {user.age}</p>
+                                <br/>
+                                <div>
+                                    <button onClick={() => props.handleDeleteUser(user.id)}>
+                                        Delete user
+                                    </button>
+                                </div>
+                                <hr/>
+                            </div>
+                        );
+                    })}
+                </>
+            )}
+        </div>
+    );
 }
+
+export default DisplayInfo;
