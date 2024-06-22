1886d855ea81adb69bae7cc6479ebfeed3320757
benphamdev <duychien152003@gmail.com>
2024-06-22 09:46:08 +0700

    react router dom

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from 'react-redux';
 import store from './redux/store';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import App from "./App";
+import User from "./components/User/User";
+import Admin from "./components/Admin/Admin";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
         <React.StrictMode>
-            <App/>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<App/>}/>
+                    <Route path="/users" element={<User/>}/>
+                    <Route path="/admins*" element={<Admin/>}/>
+                </Routes>
+            </BrowserRouter>
         </React.StrictMode>
     </Provider>
 );
