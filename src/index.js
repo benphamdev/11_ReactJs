eeb4fde11ad13663b91d2f526f797b7658b475b2
benphamdev <duychien152003@gmail.com>
2024-06-22 10:51:25 +0700

    navlink && overide active css

--- src/index.js
+++ src/index.js
@@ -17,8 +17,8 @@ root.render(
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<App/>}>
-                        <Route path="/users" element={<User/>}/>
-                        <Route path="/admins*" element={<Admin/>}/>
+                        <Route path="/users/*" element={<User/>}/>
+                        <Route path="/admins/*" element={<Admin/>}/>
                     </Route>
                 </Routes>
             </BrowserRouter>
