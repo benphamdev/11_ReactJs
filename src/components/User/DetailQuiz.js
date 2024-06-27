33c92ec5b21fc1ca120e80ef4a01016083532865
benphamdev <duychien152003@gmail.com>
2024-06-27 10:55:25 +0700

    process data : js, lodash group by : condition

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,6 +1,7 @@
 import {useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import {getQuestionByQuizId} from "../../services/api/QuizService";
+import _ from "lodash";
 
 export const DetailQuiz = () => {
     const params = useParams();
@@ -14,10 +15,42 @@ export const DetailQuiz = () => {
     const fetchQuestions = async () => {
         let response = await getQuestionByQuizId(quizId);
 
-        console.log("detail quiz", response);
-
         if (response && response.EC === 0) {
-            setQuestions(response.DT);
+            let rawQuestions = response.DT;
+
+            // way not using lodash
+            // console.log("raw questions", rawQuestions);
+            let groupQuestions = Map.groupBy(rawQuestions, question => question.id);
+            // console.log("group questions", groupQuestions);
+            // let ans = Array.from(groupQuestions, ([key, value]) => {
+            //     let answers = value.map(question => ({
+            //         answerId: question.answers.id,
+            //         description: question.answers.description,
+            //     }));
+            //     return {
+            //         questionId: key,
+            //         description: value[0].description,
+            //         image: value[0].image || '',
+            //         answers: answers,
+            //     }
+            // });
+
+            // way using lodash
+            let ans = _.chain(rawQuestions)
+                .groupBy('id')
+                .map((value, key) => {
+                    let answers = [];
+                    value.forEach((item) => answers.push(item.answers));
+                    return {
+                        questionId: key,
+                        description: value[0].description || '',
+                        image: value[0].image || '',
+                        answers: answers || [],
+                    }
+                })
+                .value();
+            console.log("ans", ans);
+            setQuestions(ans);
         }
     }
 
