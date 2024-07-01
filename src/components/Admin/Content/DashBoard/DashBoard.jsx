39c10d94a7ce27012503295c2b2785147b4e0200
benphamdev <duychien152003@gmail.com>
2024-07-01 16:49:41 +0700

    api dashboard overview

--- src/components/Admin/Content/DashBoard/DashBoard.jsx
+++ src/components/Admin/Content/DashBoard/DashBoard.jsx
@@ -1,66 +1,93 @@
 import './DashBoard.scss'
-import {Bar, BarChart, CartesianGrid, Legend, Tooltip, XAxis, YAxis} from "recharts";
+import {useEffect, useState} from "react";
+import {Bar, BarChart, CartesianGrid, Legend, ResponsiveContainer, Tooltip, XAxis, YAxis} from "recharts";
+import {getDashboardData} from "../../../../services/api/AuthService";
 
 const DashBoard = (props) => {
-    const data = [
-        {
-            "name": "Page A",
-            "uv": 4000,
-            "pv": 2400
-        },
-        {
-            "name": "Page B",
-            "uv": 3000,
-            "pv": 1398
-        },
-        {
-            "name": "Page C",
-            "uv": 2000,
-            "pv": 9800
-        },
-        {
-            "name": "Page D",
-            "uv": 2780,
-            "pv": 3908
-        },
-        {
-            "name": "Page E",
-            "uv": 1890,
-            "pv": 4800
-        },
-        {
-            "name": "Page F",
-            "uv": 2390,
-            "pv": 3800
-        },
-        {
-            "name": "Page G",
-            "uv": 3490,
-            "pv": 4300
+    const [dataOverview, setDataOverview] = useState([])
+    const [dataChart, setDataChart] = useState([]);
+
+    useEffect(() => {
+        fetchDashboardData();
+    }, []);
+
+    const fetchDashboardData = async () => {
+        let response = await getDashboardData();
+        if (response && response.EC === 0) {
+            setDataOverview(response.DT);
+            setDataChart([
+                {
+                    "name": "Quizzes",
+                    "qz": response?.DT?.others?.countQuiz ?? 0,
+                },
+                {
+                    "name": "Questions",
+                    "qs": response?.DT?.others?.countQuestions ?? 0,
+                },
+                {
+                    "name": "Answers",
+                    'as': response?.DT?.others?.countAnswers ?? 0,
+                },
+            ]);
+        } else {
+
         }
-    ]
+    }
+
     return (
         <div className="dashboard-container">
             <div className={'title'}>Welcome to the admin dashboard</div>
 
             <div className={'content'}>
                 <div className={'left-panel'}>
-                    <div className={'child'}>Total User</div>
-                    <div className={'child'}>Total Quiz</div>
-                    <div className={'child'}>Total</div>
-                    <div className={'child'}>Total</div>
+                    <div className={'child'}>
+                        <span className={'text-1'}>Total User</span>
+                        <span className={'text-2'}>
+                            {dataOverview?.users?.total ?? 10}
+                        </span>
+                    </div>
+                    <div className={'child'}>
+                        <span className={'text-1'}>
+                            Total User
+                        </span>
+                        <span className={'text-2'}>
+                            {dataOverview?.users?.countUsers ?? 10}
+                        </span>
+                    </div>
+                    <div className={'child'}>
+                        <span className={'text-1'}>
+                            Total Question
+                        </span>
+                        <span className={'text-2'}>
+                            {
+                                dataOverview?.others?.countQuestions ?? 10
+                            }
+                        </span>
+                    </div>
+                    <div className={'child'}>
+                        <span className={'text-1'}>
+                            Total answer
+                        </span>
+                        <span className={'text-2'}>
+                            {dataOverview?.others?.countAnswers ?? 10}
+                        </span>
+                    </div>
                 </div>
 
                 <div className={'right-panel'}>
-                    <BarChart width={400} height={400} data={data}>
-                        <CartesianGrid strokeDasharray="3 3"/>
-                        <XAxis dataKey="name"/>
-                        <YAxis/>
-                        <Tooltip/>
-                        <Legend/>
-                        <Bar dataKey="pv" fill="#8884d8"/>
-                        <Bar dataKey="uv" fill="#82ca9d"/>
-                    </BarChart>
+                    <ResponsiveContainer width="96%" height={"96%"}>
+                        <BarChart data={dataChart} margin={{top: 25, right: 5, left: 5, bottom: 0}}>
+                            <CartesianGrid strokeDasharray="3 3"/>
+                            <XAxis dataKey="name"/>
+                            <YAxis/>
+                            <Tooltip/>
+                            <Legend/>
+                            <Bar dataKey="qz" fill="#8884d8"/>
+                            <Bar dataKey="qs" fill="#82ca9d"/>
+                            <Bar dataKey="as" fill="#7fd12e"/>
+                        </BarChart>
+                    </ResponsiveContainer>
+
                 </div>
             </div>
         </div>
--- src/services/api/AuthService.js
+++ src/services/api/AuthService.js
@@ -3,7 +3,7 @@ import axios from "../../configs/AxiosCustomize";
 const login = (data) => {
     return axios.post("api/v1/login", data)
         .then(res => {
-            console.log("api service : ", res);
+            console.log("auth service : ", res);
             return res;
         }).catch(err => {
             console.log(err)
@@ -14,7 +14,7 @@ const login = (data) => {
 const register = (data) => {
     return axios.post("api/v1/register", data)
         .then(res => {
-            console.log("api service : ", res);
+            console.log("auth service : ", res);
             return res;
         }).catch(err => {
             console.log(err)
@@ -25,7 +25,7 @@ const register = (data) => {
 const logout = (email, refresh_token) => {
     return axios.post("api/v1/logout", {email, refresh_token})
         .then(res => {
-            console.log("api service : ", res);
+            console.log("auth service : ", res);
             return res;
         }).catch(err => {
             console.log(err)
@@ -33,6 +33,17 @@ const logout = (email, refresh_token) => {
         })
 }
 
+const getDashboardData = () => {
+    return axios.get("api/v1/overview")
+        .then(res => {
+            console.log("auth service : ", res);
+            return res;
+        }).catch(err => {
+            console.log(err)
+            return Promise.reject(err);
+        });
+}
+
 export {
-    login, register, logout
+    login, register, logout, getDashboardData
 };
